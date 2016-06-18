a92e419b64d0c5bde76e80200d0c1ef21bccf5de
Blake Plumb <blake-plumb@leavitt.com>
2016-06-18 17:05:37 -0600

    Added documentation. Going to do some final testing and then ready for pull request.

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,9 +1,13 @@
-/* This is needed to make IOS behave like all the other browser */
+/* This is needed to make IOS behave like all the other browser 
+   references
+       http://codeblog.cz/vanilla/#prerequisities
+*/
 
 var accordions = document.querySelectorAll('.accordion');
 
 [].forEach.call(accordions, function(accordion){
-	accordion.addEventListener('touchstart', function(){
+	accordion.addEventListener('click', function(){
 		accordion.focus();
+		return false;
 	});
 });
